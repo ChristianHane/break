1248618578a1e8df66136c52e8af401a4d2c472d
Christian <christian_hane@aol.com>
2018-02-13 13:35:15 -0800

    Added scroll feature for when users click the 'about' button in the navbar. Also made a slight change to class names and grid layout for the navabar

--- assets/javascript/landing-page.js
+++ assets/javascript/landing-page.js
@@ -8,4 +8,13 @@ $('#sign-up').click(() => {
   $('#sign-up').toggleClass('sign-up-selecetor');
 })
 
-$('#exit-sign-in').click(() => $('.sign-in-menu').toggle('slow'));
+$('#exit-sign-in').click(() => {
+  $('.sign-in-menu').slideToggle('medium');
+  setTimeout(() => $('.sign-in').toggleClass('sign-in-background'), 500);
+});
+
+$('.about').click(() => {
+  $('html, body').animate({
+    scrollTop: ($('#steps-container').first().offset().top)
+  },500);
+})
