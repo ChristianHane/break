78b6a648f1d0113fadc7724db7d60a0c18ce952a
Christian <christian_hane@aol.com>
2018-02-12 20:44:59 -0800

    Started styling of sign up and login form

--- assets/javascript/landing-page.js
+++ assets/javascript/landing-page.js
@@ -1,3 +1,11 @@
 $('.sign-in').on('click', function(event) {
+  event.stopPropagation();
   $('.sign-in-menu').toggle('slow');
+  $('.sign-in').toggleClass('sign-in-background');  
 })
+
+$('#sign-up').click(() => {
+  $('#sign-up').toggleClass('sign-up-selecetor');
+})
+
+$('#exit-sign-in').click(() => $('.sign-in-menu').toggle('slow'));
